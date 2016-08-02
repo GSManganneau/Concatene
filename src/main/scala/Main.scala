148c63068cd2d19e8ff86f29e7d1f80335bcf1f3
GSManganneau <manganneaustephane@gmail.com>
2016-08-02 15:41:21 +0200

    "main"

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,31 +9,32 @@ object Main{
 	val writer = new PrintWriter(new File("/home/admin/data/result.txt" ))
 
 	def concatene (line : Array[String]) : Array[String] = {
-		val result = new Array[String](line.length -1)
 
-    if (line[0] == "PATENT"){
-			result[0] = "PATENT"
-			result[1] = "NAME"
-			for (int i = 2 ; i <= line.length - 2 ; i++){
-				result[i] = line[i]
+		val result = new Array[String](line.length - 1)
+
+		if (line(0) == "PATENT") {
+			result(0) = "PATENT"
+			result(1) = "NAME"
+			for (i <- 2 to (line.length - 2)) {
+				result(i) = line(i)
 			}
 			return result
-
-	  else{
-				result[0] = line[0]
-				result[1] = line[1] + " " +line[2]
-				for (int i = 3 ; i <= line.length - 2 ; i++){
-					result[i] = line[i]
-				}
-				return result
-			}
 		}
+
+		else {
+			result(0) = line(0)
+			result(1) = line(1) + " " + line(2)
+			for (i <- 3 to (line.length - 2)) {
+
+				result(i) = line(i)
+			}
+			return result
+		}
+
 
 	}
 
-	def main (args : Array[String]){
-
-
+	def main(args : Array[String]){
 		val conf = new SparkConf().setAppName(appName).setMaster(master)
 		val sc = new SparkContext(conf)
 		val dataRdd = sc.textFile("/home/admin/data/ainventor-short.csv")
